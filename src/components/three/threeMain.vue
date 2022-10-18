4213f9ca19232ae414d2bb5eec3dd4ab6b23996f
yangshuo <yangshuo@teamsun.com.cn>
2022-10-18 20:08:10 +0800

    init repository

---
+++ src/components/three/threeMain.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { onMounted } from "vue";
+import * as THREE from "three";
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
+camera.position.set( 0, 0, 100 );
+camera.lookAt( 0, 0, 0 );
+
+const scene = new THREE.Scene();
+//create a blue LineBasicMaterial
+const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+const points = [];
+points.push( new THREE.Vector3( - 10, 0, 0 ) );
+points.push( new THREE.Vector3( 0, 10, 0 ) );
+points.push( new THREE.Vector3( 10, 0, 0 ) );
+
+const geometry = new THREE.BufferGeometry().setFromPoints( points );
+const line = new THREE.Line( geometry, material );
+
+console.log(scene);
+
+
+onMounted(() => {
+    console.log("self",scene);
+    scene.add( line );
+    renderer.render( scene, camera );
+});
+</script>
+
+<template>
+  <div style="background: skyblue;"></div>
+
+</template>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,7 @@
+<script setup>
+import ThreeMain from "../components/three/threeMain.vue"
+</script>
+
+<template>
+  <ThreeMain />
+</template>

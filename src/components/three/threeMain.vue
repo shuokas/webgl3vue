f5c1b74271ff089f82a4001b02cb79b04a023e78
shuoka <15811519008@163.com>
2022-10-19 22:59:54 +0800

    更新动画

--- src/components/three/threeMain.vue
+++ src/components/three/threeMain.vue
@@ -24,7 +24,7 @@ const initScenes = () => {
     // 创建基础网格材质时可设置颜色 new THREE.MeshBasicMaterial({color: 0x0080ff});
     const material = new THREE.MeshNormalMaterial();
     const cube = new THREE.Mesh( geometry, material );
-    cube.position.set(5,0,0);
+    cube.position.set(0,0,0);
     // 缩放
     // cube.scale.set(2,1,1);
     // 旋转
@@ -38,15 +38,51 @@ const initScenes = () => {
 
     // renderer.render( scene, camera );
     // 创建轨道控制器
-    const controls = new OrbitControls(camera, renderer.domElement)
+    const controls = new OrbitControls(camera, renderer.domElement);
+    // 设置控制器阻尼，更真实效果，必须在render函数里设置update
+    controls.enableDamping = true;
+
     // 添加坐标轴辅助器
     const axesHelper = new THREE.AxesHelper(5);
     scene.add(axesHelper);
     // 设置时钟
-    const clock = new THREE.Clock();
-    gsap.to(cube, { duration: 2.5, ease: "bounce.out", x: 1 });
+    // const clock = new THREE.Clock();
+    let animation = gsap.to(cube.position, { duration: 2.5, ease: "bounce.out", x: 5, repeat: -1, yoyo: true, onComplete: () => {
+        console.log("动画完成");
+    } });
+    window.addEventListener('dblclick', () => {
+        console.log(animation.isActive());
+        if (animation.isActive()) {
+            animation.pause();
+        }else {
+            animation.resume();
+        }
+    })
+    gsap.to(cube.rotation, { x: 2 * Math.PI , duration: 2.5, ease: "bounce.out", repeat: -1, yoyo: true });
+    // resize
+    window.addEventListener('resize', () => {
+        console.log("监听画面变化");
+        // 更新摄像头
+        camera.aspect = window.innerWidth / window.innerHeight;
+        // 更新摄像机的投影矩阵
+        camera.updateProjectionMatrix();
+        // 更新渲染器
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        // 设置渲染器的像素比
+        renderer.setPixelRatio(window.devicePixelRatio);
+    })
     // 渲染函数
     const renderFn = () => {
+        // 获取总时长
+        // let totalTime = clock.getElapsedTime();
+        // let deltaTime = clock.getDelta();
+        
+        // // console.log("时钟运行总时长",totalTime);
+        // console.log("两次获取事件间隔时间",deltaTime); // 两针的时间差
+        // let t = totalTime % 5;
+        // cube.position.x = t * 1;
+        // cube.rotation.x = t * 1;
+        controls.update();
         renderer.render(scene, camera);
         // 渲染下一帧的时候调用render函数
         requestAnimationFrame(renderFn);
